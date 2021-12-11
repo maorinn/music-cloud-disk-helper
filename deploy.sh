e7b7d4bcfec0a5fed66b9fd4cff4a5bc6d8abf69
maoring <maoring21@gmail.com>
2021-12-12 03:46:38 +0800

    fix

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,12 @@ cd client
 npm run build
 cd ../server/cmd/app
 go mod tidy
-go build -o ../../build/musicCloudBin
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ../../build/musicCloudBin
 if [ ! -n "$(ps -ef|grep musicCloudBin|grep -v grep|awk '{print $2}')" ];then
         echo "第一次运行"
-    sudo nohup ./musicCloudBin  > nohup_musicCloudBin.log 2>&1 &
+    sudo nohup ../../build/musicCloudBin  > nohup_musicCloudBin.log 2>&1 &
 else
     kill -9 $(ps -ef|grep musicCloudBin|grep -v grep|awk '{print $2}')
-    sudo nohup ./musicCloudBin  > nohup_musicCloudBin.log 2>&1 &
+    sudo nohup ../../build/musicCloudBin  > nohup_musicCloudBin.log 2>&1 &
 fi
 
