d3401e8cce402ad153e399cfdd407f9b94a661a7
maoring <maoring21@gmail.com>
2021-12-12 04:14:00 +0800

    fix

--- client/src/config/config.ts
+++ client/src/config/config.ts
@@ -1,4 +1,4 @@
-const IS_DEBUG = process.env.NODE_ENV
+const IS_DEBUG = process.env.NODE_ENV === "development"
 export const config = {
     SERVER_HOME:IS_DEBUG?"127.0.0.1:8000":"42.192.50.25:8000"
 }
