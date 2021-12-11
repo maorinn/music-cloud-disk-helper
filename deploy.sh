5835cdd309680508b5405b7b41917bdf8cd94519
maoring <maoring21@gmail.com>
2021-12-12 02:39:02 +0800

    部署脚本

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+git pull
+cd client
+npm run build
+cd ../server/cmd/app
+go mod tidy
+go build -o ../../build/musicCloudBin
+kill -9 $(ps -ef|grep musicCloudBin|grep -v grep|awk '{print $2}')
+sudo nohup ./musicCloudBin  > nohup_musicCloudBin.log 2>&1 &
+
